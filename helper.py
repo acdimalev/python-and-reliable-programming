6e12eb01f8f3819a4f847a81c2d953b330c1330c
jamin <jamin@bitlag.net>
2018-09-03 23:03:23 -0700

    style

--- helper.py
+++ helper.py
@@ -1,12 +1,21 @@
 def function(returns):
-    '''A simple function strategy for Hypothesis.'''
+    '''A simple function strategy for Hypothesis.
+
+    based on
+    https://github.com/jsverify/jsverify/blob/v0.8.3/lib/fn.js#L18
+
+    to be succeeded by
+    https://github.com/HypothesisWorks/hypothesis/issues/167
+    '''
     from frozendict import frozendict
     from hypothesis.strategies import builds, from_type, just
+
     def new_function():
         d = {}
+
         def f(*args, **kwargs):
             key = (args, frozendict(kwargs))
-            if not key in d:
+            if key not in d:
                 d[key] = from_type(returns).example()
             return d[key]
         return f
